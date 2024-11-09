157febc9170d7885406d14bcfd786bacae8954aa
Nabir14 <mushfiquefarhannabir@gmail.com>
2024-11-09 21:54:40 +0600

    v0.5 final

--- src/nexil/src/firebase.js
+++ src/nexil/src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.fapik,
-  authDomain: process.env.ad,
-  projectId: process.env.pid,
-  storageBucket: process.env.sb,
-  messagingSenderId: process.env.msid,
-  appId: process.env.aid,
-  measurementId: process.env.mid,
+  apiKey: process.env.REACT_APP_FAPIK,
+  authDomain: process.env.REACT_APP_AD,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_MSID,
+  appId: process.env.REACT_APP_AID,
+  measurementId: process.env.REACT_APP_MID,
 };
 
 // Initialize Firebase
