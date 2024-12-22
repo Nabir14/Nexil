ebb5ba45a9602597d3c5c6a02a61113ca58d0c24
Nabir14 <mushfiquefarhannabir@gmail.com>
2024-12-22 18:28:57 +0600

    private rooms

--- src/nexil/src/App.js
+++ src/nexil/src/App.js
@@ -28,7 +28,6 @@ function UserEditRoom({ setRoom }){
 		}
 	}
 
-
 	return (
 		<div className="h-screen flex justify-center items-center bg-neutral-950">
 		<form onSubmit={() =>  saveUsername()}>
@@ -46,6 +45,7 @@ function UserEditRoom({ setRoom }){
 		</form>
 		</div>
 	);
+
 }
 
 function NavHeader({ setRoom }){
@@ -101,7 +101,7 @@ function LoginAlert(){
 	);
 }
 
-const SendMessage = ({scroll, trigger}) => {
+const SendMessage = ({scroll, trigger, readDB}) => {
 	const [message, setMessage] = useState("");
 	const textFilter = new Filter();
 
@@ -113,7 +113,7 @@ const SendMessage = ({scroll, trigger}) => {
 			return;
 		}
 		const { uid, displayName, photoURL } = auth.currentUser;
-		await addDoc(collection(db, "nexil-chat-db"), {
+		await addDoc(collection(db, readDB), {
 			text: textFilter.clean(message),
 			name: displayName,
 			avatar: photoURL,
@@ -143,10 +143,10 @@ const SendMessage = ({scroll, trigger}) => {
   );
 };
 
-const MessageControls = ({ setMControl, message }) => {
+const MessageControls = ({ setMControl, message, dbName }) => {
 	const [deleteAlertBox, setDAlertBox] = useState(false)
 	const deleteMessage = async () => {
-		await deleteDoc(doc(db, "nexil-chat-db", message.id))
+		await deleteDoc(doc(db, dbName, message.id))
 		setDAlertBox(false)
 	}
 	return (
@@ -191,7 +191,7 @@ const ReceivedMessage = ({ message }) => {
   );
 };
 
-const SentMessage = ({ message }) => {
+const SentMessage = ({ message, dbName }) => {
 	const [mControl, setMControl] = useState(false)
 	const mControlTrigger = () => {
 		if(mControl === true){
@@ -202,7 +202,7 @@ const SentMessage = ({ message }) => {
 	}
 	return (
       <div className="py-1 flex justify-end text-white">
-		{mControl ? <MessageControls setMControl={setMControl} message={message} /> : null}
+		{mControl ? <MessageControls setMControl={setMControl} message={message} dbName={dbName} /> : null}
     <div className="bg-neutral-800 border-0 rounded-b-md rounded-l-md max-w-[65%] inline-block">
 	<div className="text-right px-1 py-1" onClick={mControlTrigger}>
       <p className="italic font-bold text-sm">{message.name}</p>
@@ -218,23 +218,23 @@ const SentMessage = ({ message }) => {
   );
 }
 
-const MessageInfoContainer = ({ message }) => {
+const MessageInfoContainer = ({ message, dbName }) => {
   const [user] = useAuthState(auth);
 
   return (
     <div>
-	  {message.uid === user.uid ? <SentMessage message={message} /> : <ReceivedMessage message={message}/>}
+	  {message.uid === user.uid ? <SentMessage message={message} dbName={dbName}/> : <ReceivedMessage message={message}/>}
       </div>
   );
 };
 
-const MessageBody =  ({ setRoom }) => {
+const MessageBody =  ({ setRoom, readDB }) => {
   const [messages, setMessages] = useState([]);
   const scroll = useRef();
 
   useEffect(() => {
     const q = query(
-      collection(db, "nexil-chat-db"),
+      collection(db, readDB),
       orderBy("createdAt"),
     );
 
@@ -255,14 +255,15 @@ const MessageBody =  ({ setRoom }) => {
 	  <main onLoad={() => scroll.current.scrollIntoView({ behavior: "auto" })}>
 	  <div>
 	{messages?.map((message) => (
-          <MessageInfoContainer key={message.id} message={message} />
+          <MessageInfoContainer key={message.id} message={message} dbName={readDB} />
         ))}
 	  </div>
 	  <span ref={scroll}></span>
-          <SendMessage scroll={scroll} trigger={ setRoom }/>
+          <SendMessage scroll={scroll} trigger={ setRoom } readDB={ readDB }/>
 	  </main>
   );
 };
+
 
 const PCRCard = ({ trigger }) => {
 	return (
@@ -295,19 +296,78 @@ const PCRCard = ({ trigger }) => {
 );
 }
 
-const Lobby = ({ enterRoom }) => {
+const PRCRCard = ({ setRoom, setDBName }) => {
+	const [dbCon, setDBCon] = useState(false);
+	return (
+		<div className="max-w-sm p-6 bg-white border rounded-lg shadow bg-zinc-900 border-neutral-700 transition ease-in-out hover:shadow-lg hover:shadow-fuchsia-500/50">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+      Private Chat Room
+	</h5>
+  <p className="mb-3 font-normal text-gray-400">
+    Your private space.
+  </p>
+  <button className="transition ease-in-out delay-500 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-800 to-fuchsia-500 hover:border-2 hover:border-blue-500" onClick={() => setDBCon(true)} >
+    Enter
+    <svg
+      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 14 10"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M1 5h12m0 0L9 1m4 4L9 9"
+      />
+    </svg>
+  </button>
+		{dbCon ? (
+		<div className="h-screen flex justify-center items-center bg-neutral-950">
+		<form onSubmit={() => setRoom(2)}>
+		<h1 className="text-3xl font-bold text-white text-center pb-4">Private Room</h1>
+		<div className="flex">
+			<p className="text-md font-bold text-white px-2">Room Code:</p>
+			<input type="text" id="roomCode" onChange={(e) => setDBName(e.target.value)} placeholder="your room code" />
+		</div>
+		<div className="flex justify-center items-center py-2">
+			<button className="rounded-full bg-white hover:bg-purple-900 hover:text-white font-bold px-2 py-2" type="submit">CreateDB</button>
+		<div className="px-2">
+			<button onClick={() => setRoom(0)} className="rounded-full bg-white hover:bg-purple-900 hover:text-white font-bold px-2 py-2">Cancel</button>
+		</div>
+		</div>
+		</form>
+		</div> 
+		) : null}
+</div>
+);
+}
+
+const Lobby = ({ setRoom, enterRoom, setDBName}) => {
 	return (
 		<div className="h-screen flex items-center justify-center">
 			<PCRCard trigger={() => enterRoom(1)}/>
+			<PRCRCard setRoom={setRoom} setDBName={setDBName}/>
 		</div>
 
 	);
 }
 
+
 const PublicChatRoom = ({ setRoom }) => {
 	return(
 		<div>
-			<MessageBody setRoom={setRoom} />
+			<MessageBody setRoom={setRoom} readDB="nexil-chat-db" />
+		</div>
+	);
+}
+
+const PrivateRoom = ({ setRoom, dbName }) => {
+	return(
+		<div>
+			<MessageBody setRoom={setRoom} readDB={dbName} />
 		</div>
 	);
 }
@@ -325,6 +385,7 @@ function App() {
   const [room, setRoom] = useState(0);
   const [loading, setLoading] = useState(false);
   const [targetRoom, setTargetRoom] = useState(null);
+  const [dbName, setDBName] = useState("");
 
   const enterRoom = (roomId) => {
     setTargetRoom(roomId);
@@ -345,11 +406,13 @@ function App() {
       ) : room === 10 ? (
         <UserEditRoom setRoom={setRoom} />
       ) : room === 0 ? (
-        <Lobby enterRoom={enterRoom} />
+        <Lobby setRoom={setRoom} enterRoom={enterRoom} setDBName={setDBName} />
       ) : room === 1 ? (
         <PublicChatRoom setRoom={setRoom} />
+      ) : room === 2 ? (
+	<PrivateRoom setRoom={setRoom} dbName={dbName}/>
       ) : (
-	<Lobby enterRoom={enterRoom} />
+	<Lobby setRoom={setRoom} enterRoom={enterRoom} setDBName={setDBName}/>
       )}
     </div>
   );
